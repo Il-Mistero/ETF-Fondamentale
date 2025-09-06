fdc7312cd3d2e45b295c333be30a625e392e0d70
Lo Sviluppatore Misterioso. <126560508+Il-Mistero@users.noreply.github.com>
2025-09-06 23:21:39 +0100

    Initial Commit

--- api/stocks.js
+++ api/stocks.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
       // ETF fundamentals
       navPrice: quote.summaryDetail?.navPrice || null,
-      expenseRatio: quote.defaultkeyStatistics.netExpenseRatio || null,
+      expenseRatio: quote.defaultkeyStatistics?.netExpenseRatio || null,
       totalAssets: quote.summaryDetail?.totalAssets || null,
       bidPrice: quote.summaryDetail?.bid|| null,
       askPrice: quote.summaryDetail?.ask|| null,
